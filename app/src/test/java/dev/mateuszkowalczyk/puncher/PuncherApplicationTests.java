0fc08c807bd2affbc58014ba4ad5452d5ce9a59e
MKDrwal <mkdrwal@localhost.localdomain>
2020-02-16 12:57:43 +0100

    add spring boot to project

---
+++ app/src/test/java/dev/mateuszkowalczyk/puncher/PuncherApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mateuszkowalczyk.puncher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuncherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
