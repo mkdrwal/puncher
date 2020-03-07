cee257f41f238b506142ef7e0e3daef4e1c253bc
Mateusz Kowalczyk <mateusz.kowalczyk24@gmail.com>
2020-03-07 18:40:53 +0100

    add constraint on user table

--- app/src/main/java/dev/mateuszkowalczyk/puncher/entity/User.java
+++ app/src/main/java/dev/mateuszkowalczyk/puncher/entity/User.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = {"username"})})
 public class User extends AuditModel {
 
     @Id
